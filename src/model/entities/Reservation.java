3d492c915ff62c6a9e8703266e7222fabb1aa4ac
Eduardo Elly <eduardo_elly@hotmail.com>
2020-02-12 17:04:58 -0300

    Solução ruim de exceções 2.0

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-    
+
     private Integer roomNunmber;
     private Date checkIn;
     private Date checkOut;
-    
+
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    
+
     public Reservation() {
     }
 
@@ -36,28 +36,36 @@ public class Reservation {
     public Date getCheckOut() {
         return checkOut;
     }
-    
+
     public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    
-    public void updateDates(Date checkIn, Date checkOut) {
+
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservation dates for update must be future detes!";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date!";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
-    
+
     @Override
     public String toString() {
         return "Room"
-            + roomNunmber
-            + ", check-in: "
-            + sdf.format(checkIn)
-            + ", check-out: "
-            + sdf.format(checkOut)
-            + ", "
-            + duration()
-            + " nights";
+                + roomNunmber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
     }
-    
+
 }
